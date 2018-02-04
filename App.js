1f9c398a5f274e93d0e283b9cff516ae98fa99ef
JesusBrito <jesus291196@gmail.com>
2018-02-04 17:47:40 -0600

    Primera prueba del frontend utilizando react native y flexbox para alinear tres cajas

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+ 
+export default class AlignItems extends Component {
+  render() {
+    return (
+      <View style={styles.wrapper}>
+  
+        <View style={styles.container}>
+            <View style={[styles.box, styles.box1]}></View>
+        </View>
+        <View style={styles.container2}>
+            <View style={[styles.box, styles.box2]}></View>
+        </View>
+         <View style={styles.container3}>
+
+            <View style={[styles.box, styles.box3]}></View>
+        </View>
+      </View>
+    );
+  }
+}
+ 
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
+  container: {
+    flex: .5,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  container2: {
+    flex: .5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center' 
+  },
+  container3: {
+    flex: .5,
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  box: {
+    width: 50,
+    height: 50
+  },
+  box1: {
+    backgroundColor: '#2196F3'
+  },
+  box2: {
+    backgroundColor: '#8BC34A'
+  },
+  box3: {
+    backgroundColor: '#e3aa1a'
+  }
+});
